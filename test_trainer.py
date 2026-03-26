8fc3762b07f4b618460cd4ce378dee23ce022fb4
skookumer <ruminator@DESKTOP-41LQTGQ.localdomain>
2026-03-26 11:34:55 -0400

    fixed training loop

--- test_trainer.py
+++ test_trainer.py
@@ -1,21 +1,21 @@
 from aligner import GeneLoader
 from HMM import HMM
-import time
+import numpy as np
 
 
 l = GeneLoader("hg38_primary")
-hmm_sv = HMM(n_states=len(l.states), k=6, n_bases=6, model_name="sv_a", fresh_start=True)
-# hmm_bm = HMM(n_states=len(l.states) * 2, k=6)
 
-t0 = time.time()
-for i in range(1, 11):
-    seq, conf, states = l.get_idx(i)
-    seq, states = hmm_sv.tokenize(seq, states)
-    A, E = hmm_sv.MLE_E(seq, states)
-    hmm_sv.MLE_M(A, E)
-    print(f"iteration {i}, time {time.time() - t0}")
-    t0 = time.time()
-    pred = hmm_sv.viterbi(seq)
-    print(states[:20])
-    print(pred[:20])
-    input()
+hmm_sv = HMM(n_states=len(l.states), k=6, n_bases=6, model_name="supervised_10k_oldschema", fresh_start=True)
+
+A = np.ones((hmm_sv.n_states, hmm_sv.n_states))
+E = np.ones((hmm_sv.n_states, hmm_sv.vocab_size))
+for i in range(1, 10000):
+    entry = l[i]
+    seq, states = hmm_sv.tokenize(entry["seq"], entry["states"])
+    A_sub, E_sub = hmm_sv.MLE_E(seq, states)
+    A += A_sub
+    E += E_sub
+    print(f"iteration {i}        ", end='\r', flush=True)
+hmm_sv.MLE_M(A, E)
+hmm_sv.save_matrices()
+
