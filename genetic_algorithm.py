506799edf0c7b39cdebca649796650738ada6af6
skookumer <ruminator@DESKTOP-41LQTGQ.localdomain>
2026-03-28 14:10:50 -0400

    many features added

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -4,6 +4,10 @@ import Levenshtein
 from pathlib import Path
 import polars as pl
 import polars.selectors as cs
+from numba import njit
+from numba.typed import List as JitList
+from numba import int32, typeof
+import torch
 
 home = Path(__file__).parent
 data_path = home / "popns"
@@ -13,6 +17,11 @@ objectives = {
     "ham_dist": {"reverse": False},
     "hmm_prob": {"reverse": False}
 }
+
+
+'''
+look into BLOSUM scores for substitution severity; scale phylop scores by this?
+'''
 
 
 class NSGA_II:
@@ -48,25 +57,29 @@ class NSGA_II:
         self.df = pl.DataFrame()
         self.state_choices = np.array(self.loader.states)
         self.k = self.hmm.k
+        self.master = self.make_master_dict()
 
+    def make_master_dict(self):
+        data = self.loader.get_idx(self.idx, with_row=True)
+        return {"annot_idx": self.idx,
+                 "raw_idx": data["start"],
+                 "seq": list(data["seq"]),
+                 "states": data["states"],
+                 "conv_array": data["conv"],
+                 "embedding": self._encode([data["seq"]])[0],
+                 "ll": self.get_likelihood_only(data["seq"])}
+        
 
     def gen_entry(self):
-        data = self.loader.get_idx(self.idx, with_row=True)
-        entry = {"annot_idx": self.idx,
-                 "raw_idx": data["start"],
-                 "orig_seq": list(data["seq"]),
-                 "orig_states": data["states"],
-                 "orig_conv": data["conv"],
-                 "orig_embedding": self.get_embedding(data["seq"]),
-                 "orig_ll": self.get_likelihood_only(data["seq"]),
-                 "mod_seq": list(data["seq"]),
-                 "mod_states": data["states"].copy(),
-                 "mod_conv_score": data["conv"].copy(),
-                 "mod_embedding": self.get_embedding if self.model != None else None,
-                 "mut_array": np.zeros(len(data["seq"])),
-                 "mod_ll": -np.inf,
+        entry = {"seq": self.master["seq"].copy(),
+                 "states": self.master["states"].copy(),
+                 "conv_score": 0,
+                 "embedding": None,
+                 "mut_array": np.zeros(len(self.master["seq"]), dtype=np.uint8),
+                 "ll": -np.inf,
                  "fitness": {key: 0 for key in self.objectives},
-                 "live": True}
+                 "live": True,
+                 "update": True}
         return entry
     
     def get_likelihood_only(self, seq):
@@ -74,13 +87,53 @@ class NSGA_II:
         p_x = self.hmm.ll_only(enc_seq)
         return p_x
 
-    def get_embedding(self, seq):
+    # def _encode(self, seqs):
+    #     if self.model is not None and self.tokenizer is not None:
+    #         inputs = self.tokenizer(seqs, return_tensors='pt', padding=True, truncation=True)["input_ids"]
+    #         with torch.no_grad():
+    #             hidden_states = self.model(inputs)[0] # [1, sequence_length, 768]
+    #         embedding_mean = hidden_states[0].detach().numpy().mean(axis=0)
+    #         return embedding_mean
+    #     return None
+
+    def _encode(self, seqs):
         if self.model is not None and self.tokenizer is not None:
-            inputs = self.tokenizer(seq, return_tensors = 'pt')["input_ids"]
-            hidden_states = self.model(inputs)[0] # [1, sequence_length, 768]
-            embedding_mean = hidden_states[0].detach().numpy().mean(axis=0)
-            return embedding_mean
+            embeddings = []
+            for seq in seqs:
+                inputs = self.tokenizer(seq, return_tensors='pt')["input_ids"]
+                hidden_states = self.model(inputs)[0]
+                embeddings.append(hidden_states[0].detach().numpy().mean(axis=0))
+            return np.array(embeddings)
         return None
+
+    def get_embeddings(self):
+        indices = [i for i in range(len(self.popn)) if self.popn[i]["live"] and self.popn[i]["update"]]
+        seqs = ["".join(self.popn[i]["seq"]) for i in indices]
+        embeddings = self._encode(seqs)
+        if embeddings is not None:
+            for i, idx in enumerate(indices):
+                self.popn[idx]["embedding"] = embeddings[i]
+    
+    def get_mod_ll(self):
+        indices = [i for i in range(len(self.popn)) if self.popn[i]["live"] and self.popn[i]["update"]]
+        lls = [self.get_likelihood_only(self.popn[i]["seq"]) for i in indices]
+        for i, idx in enumerate(indices):
+            self.popn[idx]["ll"] = lls[i]
+    
+    def get_mod_conv_score(self):
+        indices = [i for i in range(len(self.popn)) if self.popn[i]["live"] and self.popn[i]["update"]]
+        masks = [np.where(self.popn[i]["mut_array"] > 0)[0] for i in indices]
+        scores = [self.master["conv_array"][masks[i]].sum() / self.master["conv_array"].sum() for i, idx in enumerate(indices)]
+        for i, idx in enumerate(indices):
+            self.popn[idx]["conv_score"] = scores[i]
+    
+    def set_update_flag(self):
+        for entry in self.popn:
+            entry["update"] = False
+
+
+
+        
 
     def load_popn(self):
         if self.popn_path.exists():
@@ -111,84 +164,170 @@ class NSGA_II:
         self.df = pl.concat([self.df, current_popn])
         if write:
             self.df.write_parquet(self.popn_path)
+        
+    @staticmethod
+    @njit(cache=True)
+    def fast_sort(fit_matrix, pop_cap):
+        N = fit_matrix.shape[0]
+
+        #compute dominance relationships and first front
+        inner_list = typeof(JitList.empty_list(int32))
+        fronts = JitList.empty_list(inner_list)
+        first_front = JitList.empty_list(int32)
+        fronts.append(first_front)
+
+        dom_counts = np.zeros(N, dtype=np.int32)
+        dom_matrix = np.zeros((N, N), dtype=bool)
+        for i in range(N):
+            p = fit_matrix[i, :]
+            for j in range(i + 1, N):
+                q = fit_matrix[j, :]
+                if np.all(p > q):
+                    dom_matrix[i][j] = True
+                    dom_counts[j] += 1
+                elif np.all(q > p):
+                    dom_matrix[j][i] = True
+                    dom_counts[i] += 1
+            if dom_counts[i] == 0:
+                fronts[0].append(i)
+        
+        #find next front
+        i = 0
+        while len(fronts[i]) > 0:
+            front = JitList.empty_list(int32)
+            for j in range(len(fronts[i])):
+                p = fronts[i][j]
+                p_dominates_q = np.where(dom_matrix[p, :])[0]
+                for k in range(len(p_dominates_q)):
+                    q = p_dominates_q[k]
+                    dom_counts[q] -= 1
+                    if dom_counts[q] == 0:
+                        front.append(q)
+            i += 1
+            fronts.append(front)
+
+        #concat individuals and 
+        sorted_individuals = JitList.empty_list(int32)
+        for i in range(len(fronts)):
+            if len(sorted_individuals) + len(fronts[i]) < pop_cap:
+                for j in range(len(fronts[i])):
+                    sorted_individuals.append(fronts[i][j])
+            else:
+                n_slots = pop_cap - len(sorted_individuals)
+                if len(fronts[i]) <= 2:
+                    for k in range(min(n_slots, len(fronts[i]))):
+                        sorted_individuals.append(fronts[i][k])
+                    return np.array(sorted_individuals), 0, JitList.empty_list(int32)
+                else:
+                    return np.array(sorted_individuals), n_slots, fronts[i]
+        return np.array(sorted_individuals), 0, JitList.empty_list(int32)
+    
+    @staticmethod
+    @njit(cache=True)
+    def crowding_distance(fit_matrix, n_slots, last_front):
+        last_front = np.array(last_front, dtype=np.int32)
+        T = fit_matrix.shape[0]
+        C = fit_matrix.shape[1]
+        dists = np.zeros(T, dtype=np.float64)
+        for c in range(C):
+            col = fit_matrix[:, c]
+            sorted_row_indices = np.argsort(col)
+
+            highest = sorted_row_indices[-1]
+            lowest = sorted_row_indices[0]
+            max_range = fit_matrix[highest, c] - fit_matrix[lowest, c]
+            if max_range != 0:
+                dists[highest] = np.inf
+                dists[lowest] = np.inf
+                for i in range(1, T - 1):
+                    p = sorted_row_indices[i - 1]
+                    q = sorted_row_indices[i + 1]
+                    r = sorted_row_indices[i]
+                    if dists[r] != np.inf:
+                        dists[r] += (fit_matrix[q, c] - fit_matrix[p, c]) / max_range
+        sorted_indices = np.argsort(dists)[::-1][:n_slots]
+        return last_front[sorted_indices]
     
     def non_dominated_sort(self):
-        fitness_arrays = [
-            (i, np.array([
-                -entry["fitness"][o] if self.objectives[o]["reversed"] else entry["fitness"][o] for o in self.objectives
-            ]))
-            for i, entry in enumerate(self.popn)
-        ]
-
+                
+        fit_matrix = np.array([np.array([
+            -entry["fitness"][o] if self.objectives[o]["reversed"] else entry["fitness"][o] for o in self.objectives
+            ]) for entry in self.popn])
+        
+        sorted_individuals, n_slots, last_front = self.fast_sort(fit_matrix, self.pop_cap)
+        if n_slots > 0:
+            last_front = self.crowding_distance(fit_matrix[last_front, :], n_slots, last_front)
+            sorted_individuals = np.concatenate([sorted_individuals, last_front])
+        return sorted_individuals #an array of sorted individual indices
         
 
-
-
-    
     def truncate_popn(self):
         if len(self.popn) > self.pop_cap:
             survivor_indices = self.non_dominated_sort()
             unalive = list(set(range(len(self.popn))) - set(survivor_indices))
+            for idx in unalive:
+                self.popn[idx]["live"] = False
             self.save_popn(unalive)
             for i in sorted(unalive, reverse=True):
                 self.popn.pop(i)
+            
 
-
-
-
-        
-    
     def cos_dist(self, entry):
-        A = entry["orig_embedding"]
-        B = entry["mod_embedding"]
+        A = self.master["embedding"]
+        B = entry["embedding"]
         norm_a = A / np.linalg.norm(A)
         norm_b = B / np.linalg.norm(B)
         cdist = 1 - np.dot(norm_a, norm_b)
         return cdist
     
     def ham_dist(self, entry):
-        return hamming (list(entry["orig_seq"]), list(entry["mod_seq"]))
+        return hamming (list(self.master["seq"]), list(entry["seq"]))
 
     def lev_dist(self, entry):
-        return Levenshtein.distance(entry["orig_seq"], entry["mod_seq"])
+        return Levenshtein.distance(self.master["seq"], entry["seq"])
     
     def hmm_prob(self, entry):
-        return entry["mod_ll"]
+        return entry["ll"]
     
     def conv_dev(self, entry):
-        total = entry["orig_conv"].sum()
-        mut_score = entry["orig_conv"][entry["mod_array"] != 0]
+        total = self.master["conv_array"].sum()
+        mut_score = self.master["conv_array"][entry["mut_array"] != 0]
         return mut_score / total
     
     def conv_sum(self, entry):
-        return entry["orig_conv"][entry["mod_array"] != 0]
+        return self.master["conv_array"][entry["mut_array"] != 0]
 
     def get_fitness(self, entry):
         for objective in self.objectives:
             entry["fitness"][objective] = self.fitness_functions[objective](entry)
+    
+    def compute_total_fitness(self):
+        for i in range(len(self.popn)):
+            if self.popn[i]["live"] == True and self.popn[i]["update"] == True:
+                self.get_fitness(self.popn[i])
 
     def mutate_prefix(self, mut_idx, idx):
-        lmer = self.popn[idx]["mod_seq"][mut_idx - self.k + 1:mut_idx]
-        kmer = self.popn[idx]["mod_seq"][mut_idx - self.k + 1:mut_idx + 1]
-        nuc, state = self.hmm.prefixed_gen(lmer, kmer, self.popn[idx]["orig_states"][mut_idx], self.popn[idx]["orig_states"][mut_idx - 1])
+        lmer = self.popn[idx]["seq"][mut_idx - self.k + 1:mut_idx]
+        kmer = self.popn[idx]["seq"][mut_idx - self.k + 1:mut_idx + 1]
+        nuc, state = self.hmm.prefixed_gen(lmer, kmer, self.master["states"][mut_idx], self.master["states"][mut_idx - 1])
         return nuc, state
     
     def update_arrays(self, nucleotide, state, mut_idx, idx):
-        self.popn[idx]["mod_seq"][mut_idx] = nucleotide
-        self.popn[idx]["mod_states"][mut_idx] = state
+        self.popn[idx]["seq"][mut_idx] = nucleotide
+        self.popn[idx]["states"][mut_idx] = state
         self.popn[idx]["mut_array"][mut_idx] = 1
     
     def revise_next(self, mut_idx, idx):
-        state = self.popn[idx]["mod_states"][mut_idx - 1]
-        lmer = self.popn[idx]["mod_seq"][mut_idx - self.k + 2:mut_idx + 1]
+        state = self.popn[idx]["states"][mut_idx - 1]
+        lmer = self.popn[idx]["seq"][mut_idx - self.k + 2:mut_idx + 1]
         nuc = self.hmm.fixed_state_gen(lmer, state)
         self.update_arrays(nuc, state, mut_idx)
 
     def check_next(self, mut_idx, idx):
         mut_idx += 1
-        if mut_idx < len(self.popn[idx]["mod_seq"]):
-            state = self.popn[idx]["mod_states"][mut_idx]
-            kmer = self.popn[idx]["mod_seq"][mut_idx - self.k + 1:mut_idx + 1]
+        if mut_idx < len(self.popn[idx]["seq"]):
+            state = self.popn[idx]["states"][mut_idx]
+            kmer = self.popn[idx]["seq"][mut_idx - self.k + 1:mut_idx + 1]
             return self.hmm.check_next_nucleotide(kmer, state)
         return False
 
@@ -218,9 +357,10 @@ class NSGA_II:
             mut_idx = indices[x]
     
             mut_idx = mutate(mut_idx, x)
-            while mut_idx + 1 < len(self.popn[idx]["mod_seq"]) and self.check_next(mut_idx, idx) == False:
+            while mut_idx + 1 < len(self.popn[idx]["seq"]) and self.check_next(mut_idx, idx) == False:
                 self.revise_next(mut_idx, idx)
                 mut_idx += 1
+            self.popn[idx]["update"] = True
         
         # self.get_fitness(self.popn[idx])
     
@@ -244,19 +384,18 @@ class NSGA_II:
         
         def make_child(n, o, m):
             child = self.gen_entry()
-            child["mod_seq"] = child["orig_seq"][:self.k - 1] + n
-            child["mod_states"] = o
+            child["seq"] = self.master["seq"][:self.k - 1] + n
+            child["states"] = o
             child["mut_array"] = m
-            child["mod_ll"] = self.get_likelihood_only(child["mod_seq"])
-            child["mod_embedding"] = self.get_embedding if self.model != None else None
+            child["update"] = True
             return child
 
 
-        seq1 = self.popn[idx1]["mod_seq"].copy()
-        seq2 = self.popn[idx2]["mod_seq"].copy()
+        seq1 = self.popn[idx1]["seq"].copy()
+        seq2 = self.popn[idx2]["seq"].copy()
 
-        state1 = self.popn[idx1]["mod_states"].copy()
-        state2 = self.popn[idx2]["mod_states"].copy()
+        state1 = self.popn[idx1]["states"].copy()
+        state2 = self.popn[idx2]["states"].copy()
 
         mut1 = self.popn[idx1]["mut_array"].copy()
         mut2 = self.popn[idx2]["mut_array"].copy()
@@ -264,11 +403,23 @@ class NSGA_II:
         T = len(seq1)
         M = len(mut1)
 
-        points = [self.k - 1] + sorted(np.random.choice(range(self.k - 1, T), seg, replace=False)) + [T]
+        choice_array = np.arange(T)
+        mask = np.full(len(choice_array), True, dtype=bool)
+        mask[0: self.k] = False
+        mask[T - self.k: T] = False
+        choices = []
+        for i in range(seg):
+            idx = np.random.choice(choice_array[mask])
+            choices.append(idx)
+            mask[max(0, idx - self.k): min(T, idx + self.k + 1)] = False
+
+        points = [self.k - 1] + sorted(choices) + [T]
 
         n1, n2 = [], []
         m1, m2 = np.zeros(M, dtype=np.uint8), np.zeros(M, dtype=np.uint8)
-        o1, o2 = np.zeros(M, dtype=np.uint8), np.zeros(M, dtype=np.uint8)
+        state_prefix = self.master["states"][:self.k - 1]
+        o1 = np.concatenate([state_prefix, np.zeros(M - self.k + 1, dtype=np.uint8)])
+        o2 = np.concatenate([state_prefix, np.zeros(M - self.k + 1, dtype=np.uint8)])
         for i in range(len(points) - 1):
             if i % 2 == 0:
                 s1, s2 = seq1, seq2
